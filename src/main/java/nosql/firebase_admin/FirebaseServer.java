aec6f82481599c3e03bd1cb0af7dffad4dfd513c
serhanozbey <ozbey.serhan@gmail.com>
2018-05-08 16:06:31 +0200

    1.0.6 quickstart-java link

--- src/main/java/nosql/firebase_admin/FirebaseServer.java
+++ src/main/java/nosql/firebase_admin/FirebaseServer.java
@@ -4,7 +4,7 @@ public class FirebaseServer {
     
     //Deploying a servlet to Firebase App Engine
     /*https://cloud.google.com/solutions/mobile/mobile-firebase-app-engine-flexible*/
-    
+    //TODO: https://github.com/firebase/quickstart-java
     /*https://cloud.google.com/appengine/docs/java/*/
     
 }
