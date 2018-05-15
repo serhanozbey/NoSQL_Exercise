99040d03f5f264b0b608eaafd0512ddd7e6e0a4a
serhanozbey <ozbey.serhan@gmail.com>
2018-05-15 11:42:41 +0200

    1.0.7 adding modularity and scanners for creations

--- src/main/java/nosql/mongoDBExercise3/PostCreate.java
+++ src/main/java/nosql/mongoDBExercise3/PostCreate.java
@@ -1,17 +1,13 @@
 package nosql.mongoDBExercise3;
 
 import com.mongodb.*;
-import com.mongodb.operation.UpdateOperation;
 import org.bson.types.ObjectId;
 import org.mongodb.morphia.Datastore;
-import org.mongodb.morphia.InsertOptions;
 import org.mongodb.morphia.Morphia;
-import org.mongodb.morphia.UpdateOptions;
 import org.mongodb.morphia.query.Query;
 import org.mongodb.morphia.query.UpdateOperations;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 /*Database configured with added index of unique username*/
 public class PostCreate {
@@ -25,27 +21,29 @@ public class PostCreate {
         morphia = new Morphia();
         datastore = morphia.mapPackage("nosql.mongoDBExercise2").createDatastore(new MongoClient(mongoURI), "admin");
         
-        User user = new User("deniz", "deniz@gmail.com");
-        try {
-            datastore.save(user);
-        } catch (DuplicateKeyException e) {
-            //if exists, gets and assigns proper userId to user.
-            user.id = datastore.find(User.class).field("username").equal("deniz").get().id;
-        }
+        Scanner scanner = new Scanner(System.in);
+    
+        User user = saveOrGetUser(scanner);
         
         //saving to admin.posts
-        Post post = new Post(user, "melabaa");
-        datastore.save(post);
+        Post post = savePost(scanner, user);
         
         //saving to admin.user-posts
+        saveUserPost(user, post);
+        
+        //saving post comments
+        savePostComment(scanner, user, post);
+        
+    }
+    
+    private static void saveUserPost(User user, Post post) {
         Query<UserPost> currentUserPost = datastore.find(UserPost.class, "_id", user.id).disableValidation();
-        //creating with checking if theres already userPost existing
+        //creating with checking if theres already posts of that user existing
         UserPost userPost;
-        if (currentUserPost != null) {
+        if (currentUserPost.get() != null) {
             userPost = currentUserPost.get();
         } else {
             userPost = new UserPost(post);
-            
         }
         
         userPost.addPost(post);
@@ -53,28 +51,49 @@ public class PostCreate {
                 .createUpdateOperations(UserPost.class)
                 .addToSet("posts", post);
         datastore.update(currentUserPost, ops, true);
-        
-        //saving post comments
-        post = datastore.find(Post.class, "id", new ObjectId("5af45904dad2243dd434245c")).get();
-        
-        Comment comment = new Comment(user, "sanada melaba");
+    }
     
+    private static Post savePost(Scanner scanner, User user) {
+        System.out.println("Enter post");
+        Post post = new Post(user, scanner.nextLine());
+        datastore.save(post);
+        return post;
+    }
+    
+    private static User saveOrGetUser(Scanner scanner) {
+        System.out.println("Enter username name, email");
+        String username = scanner.nextLine();
+        String email = scanner.nextLine();
+        User user = new User(username,email);
+        try {
+            datastore.save(user);
+        } catch (DuplicateKeyException e) {
+            //if exists, gets and assigns proper userId to user.
+            user.id = datastore.find(User.class).field("username").equal(username).get().id;
+        }
+        return user;
+    }
+    
+    private static void savePostComment(Scanner scanner, User user, Post post) {
+        post = datastore.find(Post.class, "id", new ObjectId(post.id.toHexString())).get();
+        System.out.println("Enter first comment to your post");
+        Comment comment = new Comment(user, scanner.nextLine());
+        
         Query<PostComments> currentPostComments = datastore.find(PostComments.class, "_id", post.id).disableValidation();
         
         PostComments pc;
-    
+        
         if (currentPostComments.get() != null) {
             pc = currentPostComments.get();
         } else {
-            pc = new PostComments(post,comment);
+            pc = new PostComments(post, comment);
         }
-    
+        
         pc.addComment(comment);
         UpdateOperations ops2 = datastore
                 .createUpdateOperations(PostComments.class)
                 .addToSet("comments", comment);
         datastore.update(currentPostComments, ops2, true);
-        
     }
     
 }
