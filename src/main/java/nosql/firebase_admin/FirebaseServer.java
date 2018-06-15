25cdd6404c3728a05018daf9a59183a892dd0fb6
serhanozbey <ozbey.serhan@gmail.com>
2018-06-15 13:32:09 +0200

    1.0.18 Firestore field transactions

--- src/main/java/nosql/firebase_admin/FirebaseServer.java
+++ src/main/java/nosql/firebase_admin/FirebaseServer.java
@@ -6,14 +6,15 @@ import com.google.cloud.firestore.*;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
 import com.google.firebase.cloud.FirestoreClient;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 //just download the service account json inside the project and you're ready to go.
@@ -25,23 +26,23 @@ public class FirebaseServer {
     /*https://cloud.google.com/appengine/docs/java/*/
     
     private static Firestore mFirestore;
-    private static FirebaseAuth mAuth;
+    private static UserRecord mUser;
     private static ListenerRegistration mReg;
     private static DocumentReference mRef;
     
-    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException, FirebaseAuthException {
         FileInputStream serviceAccount =
-                new FileInputStream("...../-firebase-adminsdk-trfke-6412dfe59c.json");
+                new FileInputStream("/..");
     
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://...com")
+                .setDatabaseUrl("...")
                 .build();
     
         
         FirebaseApp.initializeApp(options);
         mFirestore = FirestoreClient.getFirestore();
-        
+        mUser = FirebaseAuth.getInstance().getUser("66VJ5VWmmAfoAXFyVXhmMjD1rLl2");
         //retrieving all of the documents inside the collection.
         
         CollectionReference users = mFirestore.collection("posts");
@@ -53,15 +54,76 @@ public class FirebaseServer {
         while (it.hasNext()) {
             System.out.println(it.next().getData());
         }
-        
-        //retrieving single document
+    
+        //TODO: retrieve a single document
         
         /*DocumentReference user = mFirestore.collection("posts").document("9FhJYNQ9nBuo6gHmWYji");
         
         ApiFuture<DocumentSnapshot> future = user.get();
         
         System.out.println(future.get().getData());*/
+        
+        //TODO: Perform a transaction. with rollback and logging. 1 Random fields, 2 An Object.
+        
+        
+        //TRANSACTION, WITHOUT OBJECT, SOME FIELDS
+        DocumentReference pushRef = mFirestore.collection("transactionPractise").document("ChQueQ0fMkiPLUUeGXyD");
+        
+        ApiFuture<Void> transaction = mFirestore.runTransaction(new Transaction.Function<Void>() {
+            @Override
+            public Void updateCallback(Transaction transaction) throws Exception {
+                DocumentSnapshot snapshot = transaction.get(pushRef).get();
+                //these fields are not validated, but when we work with objects, it will be easier to validate
+                long counter=0;
+                Map<String, Object> map = new HashMap<>();
+                //if doesn't exist, create and quit (transaction)
+                if (snapshot.getData()==null) {
+                    String name = mUser.getDisplayName();
+                    String email = mUser.getEmail();
+                    map.put("name", name.split(" ")[0]);
+                    map.put("email", email.split("@")[0]);
+                    map.put("counter", 1);
+                    transaction.set(pushRef, map);
+                    return null;
+                } else {
+                    counter = snapshot.getLong("counter");
+                }
+                
+                //update with transaction, if ANY DATA AT DOCUMENT exists
+                System.out.println(counter);
+                String coname = snapshot.get("name").toString();
+                String conemail = snapshot.get("email").toString();
+                
+                map.put("name", coname.concat("oglu"));
+                map.put("email", conemail.concat(String.valueOf(counter)));
+                map.put("counter", ++counter);
+                transaction.update(pushRef,map);
+                return null;
+            }
+        });
+        transaction.get();
+        
+        //TRANSACTION WITH OBJECT (VALIDATION PROPERTIES)
+    
+       /* DocumentReference objectRef = mFirestore.collection("transactionPractise").document("exercise2").collection("")
+        
+        ApiFuture<Void> transaction2 = mFirestore.runTransaction(new Transaction.Function<Void>() {
+            @Override
+            public Void updateCallback(Transaction transaction) throws Exception {
+                
+                return null;
+            }
+        });
+        transaction2.get();
+    
+    
+        //TODO: Perform a batched write.*/
+        
+        
+       
     }
+    
+    
     
     
 }
